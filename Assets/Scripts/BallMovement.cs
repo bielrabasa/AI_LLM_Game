de7d151776b64243d8a11ecab43f2761766b7870
Biel Rabasa <bielrabasa@gmail.com>
2024-05-18 13:42:36 +0200

    PowerUp Chaos & Rearange

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public float initialSpeed = 5f;
+    delegate void AbilityActivation();
+    List<AbilityActivation> ability = new List<AbilityActivation>();
+    int carrying = 0;
+    bool isExplosive = false;
+    bool isChaotic = false;
 
-    private Rigidbody rb;
+    public float initialSpeed = 5f;
+    Rigidbody rb;
+
+    Material mat;
+    public Material powerMat;
 
     void Start()
     {
+        carrying = 0;
+        isExplosive = false;
+        isChaotic = false;
+        ability.Clear();
+        ability.Add(RearangeBricks);
+        ability.Add(ShootRay);
+        ability.Add(ChaosMode);
+        ability.Add(ExplosiveMode);
+
         rb = GetComponent<Rigidbody>();
+        mat = GetComponent<Renderer>().material;
 
         // Start the ball movement with a random direction
         Vector3 initialDirection = new Vector2(Random.Range(-1f, 1f), 1f).normalized;
@@ -20,13 +39,89 @@ public class BallMovement : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Platform
+        if (collision.gameObject.CompareTag("PLATFORM"))
+        {
+            if (carrying != 0)
+            {
+                ability[carrying - 1]();
+                carrying = 0;
+
+                GetComponent<Renderer>().material = mat;
+            }
+        }
+
+        if(isExplosive) HandleExplosive();
+        if(isChaotic) HandleChaotic();
+
+        //Break Blocks
         if (collision.gameObject.CompareTag("BLOCK"))
         {
             Destroy(collision.gameObject);
         }
-        /*// Reflect the ball's velocity based on the collision normal
-        Debug.Log(rb.velocity);
-        Vector3 reflectDirection = Vector3.Reflect(rb.velocity.normalized, collision.contacts[0].normal);
-        rb.velocity = reflectDirection * initialSpeed;*/
+        else if (collision.gameObject.CompareTag("BLOCK_S"))
+        {
+            GetComponent<Renderer>().material = powerMat;
+
+            carrying = Random.Range(1, 5);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void HandleExplosive()
+    {
+        //TODO: Create big ball that breaks bricks arround & doesn't collide with current ball
+    }
+
+    void HandleChaotic()
+    {
+        rb.velocity = rb.velocity.normalized * initialSpeed;
+        rb.velocity *= Random.Range(0.5f, 2f);
+    }
+
+    void RearangeBricks()
+    {
+        Debug.Log("Rearange");
+        FindObjectOfType<LayoutScript>().RearangeBricks();
+    }
+    void ShootRay()
+    {
+        Debug.Log("Shoot");
+        StartCoroutine(Ray());
+    }
+
+    IEnumerator Ray()
+    {
+        yield return new WaitForSeconds(0.1f);
+        //TODO: Shoot
+    }
+
+    void ChaosMode()
+    {
+        Debug.Log("Chaos");
+        StartCoroutine(Chaotic());
+    }
+
+    IEnumerator Chaotic()
+    {
+        isChaotic = true;
+
+        yield return new WaitForSeconds(10);
+
+        isChaotic = false;
+        rb.velocity = rb.velocity.normalized * initialSpeed; //Return to normal vel
+    }
+
+    void ExplosiveMode()
+    {
+        Debug.Log("Explosive");
+        StartCoroutine(Explosive());
+    }
+
+    IEnumerator Explosive()
+    {
+        isExplosive = true;
+        yield return new WaitForSeconds(10);
+        isExplosive = false;
     }
 }
--- Assets/Scripts/LayoutScript.cs
+++ Assets/Scripts/LayoutScript.cs
@@ -1,9 +1,12 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LayoutScript : MonoBehaviour
 {
+    public Material bmat;
+    public Material umat;
     public string[] lines = new string[8];
 
     void Start()
@@ -22,10 +25,15 @@ public class LayoutScript : MonoBehaviour
                     cube.transform.localScale = new Vector3(2, 1, 1);
                     cube.transform.parent = transform;
                     pos.x += 3f;
+                    cube.GetComponent<Renderer>().material = bmat;
 
                     if (c == 'B') cube.tag = "BLOCK";
                     else if (c == 'S') cube.tag = "BLOCK_S";
-                    else if (c == 'U') cube.tag = "BLOCK_U";
+                    else if (c == 'U')
+                    {
+                        cube.tag = "BLOCK_U";
+                        cube.GetComponent<Renderer>().material = umat;
+                    }
                 }
             }
             pos.x = -15;
@@ -33,9 +41,42 @@ public class LayoutScript : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RearangeBricks();
+        }
+    }
+
+    public void RearangeBricks()
+    {
+        int vertical = Random.Range(0, 3);
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+
+            if(vertical == 1)
+            {
+                float y = child.position.y;
+                y += 2 * (23 - y); //mirror from horizontal center line
+                child.position = new Vector3(child.position.x, y, 0);
+            }
+            else if(vertical == 2)
+            {
+                float x = child.position.x;
+                x *= -1; //mirror from center
+                child.position = new Vector3(x, child.position.y, 0);
+            }
+            else
+            {
+                float y = child.position.y;
+                float x = child.position.x;
+                y += 2 * (23 - y); //mirror from horizontal center line
+                x *= -1; //mirror from center
+                child.position = new Vector3(x, y, 0);
+            }
+        }
     }
 }
